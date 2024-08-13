aeec771755583062b85c93173ae4769aae06ed53
soynain <mrhades9000@gmail.com>
2024-08-12 23:21:29 -0600

    First commit

---
+++ src/main/java/com/batchexample/main/MainApplication.java
@@ -0,0 +1,15 @@
+package com.batchexample.main;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableBatchProcessing
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
